ff3ea894097e094be8b9ee8fa631dcc474752b6c
pradeep491 <k.pradeep0491@gmail.com>
2024-12-29 17:03:41 +0530

    changes

---
+++ SpringDataJpaEx1/src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -0,0 +1,49 @@
+package com.springdatajpa.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(
+        name = "products",
+        schema = "javaguides_udemy_jpa",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "sku_unique",
+                        columnNames = "stock_keeping_unit"
+                )
+        }
+)
+public class Product {
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_generator"
+    )
+    @SequenceGenerator(
+            name = "product_generator",
+            sequenceName = "product_sequence_name",
+            allocationSize = 1
+    )
+    private long id;
+    @Column(name = "stock_keeping_unit", nullable = false)
+    private String sku;//stock keeping unit
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageUrl;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+}
